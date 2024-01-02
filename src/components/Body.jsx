8199a74126c40c3aa4a0fa20ab066fa7784943ae
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-01-02 15:07:15 +0530

    Filter Feature applied Successfully

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,40 @@
 import ResCard from "./ResCard";
 import "./Body.css";
+import { useState } from "react";
 
 const Body = (data) => {
-  const resList = data.data[0].card.card.gridElements.infoWithStyle.restaurants;
-  console.log(resList);
+  const allResList =
+    data.data[0].card.card.gridElements.infoWithStyle.restaurants;
+  const [displayResList, setDisplayResList] = useState(allResList);
+  console.log(allResList[2].info.avgRating);
+  const filterTopRatedRestraunts = () => {
+    const filteredRestraunts = allResList.filter(
+      (res) => res.info.avgRating >= 4
+    );
+    console.log(filteredRestraunts);
+    setDisplayResList(filteredRestraunts);
+  };
+
+  const showAllRestraunts = () => {
+    setDisplayResList(allResList);
+  };
+
   return (
     <div className="body">
-      <div className="card-container container">
-        {resList.map((res) => (
-          <ResCard key={res.info.id} res={res} />
-        ))}
+      <div className="container">
+        <div className="filter">
+          <button className="filter-btn" onClick={showAllRestraunts}>
+            All Restraunts
+          </button>
+          <button className="filter-btn" onClick={filterTopRatedRestraunts}>
+            Top Rated
+          </button>
+        </div>
+        <div className="card-container ">
+          {displayResList.map((res) => (
+            <ResCard key={res.info.id} res={res} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/ResCard.jsx
+++ src/components/ResCard.jsx
@@ -2,13 +2,16 @@
 import "./ResCard.css";
 
 const ResCard = ({ res }) => {
-  const imgId = res.info.cloudinaryImageId;
-  const resName = res.info.name;
-  const cuisines = res.info.cuisines.join(", ");
-  const areaName = res.info.areaName;
-  const discountHeader = res.info.aggregatedDiscountInfoV3.header;
-  const discountSubHeader = res.info.aggregatedDiscountInfoV3.subHeader;
+  console.log(res);
+  const imgId = res?.info?.cloudinaryImageId;
+  const resName = res?.info?.name;
+  const cuisines = res?.info?.cuisines?.join(", ");
+  const areaName = res?.info?.areaName;
+  const discountHeader = res?.info?.aggregatedDiscountInfoV3?.header;
+  const discountSubHeader = res?.info?.aggregatedDiscountInfoV3?.subHeader;
   const discountText = discountHeader + " " + discountSubHeader;
+  const avgRating = res?.info?.avgRating ?? "4.0";
+  const deliveryTime = res?.info?.sla?.deliveryTime ?? "30";
   return (
     <div className="res-card">
       <div className="res-img-box">
@@ -24,10 +27,10 @@ const ResCard = ({ res }) => {
       <div className="res-stats">
         <div className="rating-stat">
           <span className="star">⭐</span>
-          <span className="rating">3.8</span>
+          <span className="rating">{avgRating}</span>
         </div>
         <span className="dot">|</span>
-        <eta-stat>25 mins</eta-stat>
+        <eta-stat>{deliveryTime} mins</eta-stat>
       </div>
       <p className="cuisines">{cuisines}</p>
       <address className="location">{areaName}</address>
